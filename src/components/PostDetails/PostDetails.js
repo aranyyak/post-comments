712efbcd2f795cc31f09319a70b734daad17274d
aranyyak <aranyak.amin@gmail.com>
2022-03-09 12:00:31 +0600

    done

--- src/components/PostDetails/PostDetails.js
+++ src/components/PostDetails/PostDetails.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-import Comments from './Comments/Comments';
+import Comments from '../Comments/Comments';
 
 const PostDetails = () => {
     const [details, setDetails] = useState({})
